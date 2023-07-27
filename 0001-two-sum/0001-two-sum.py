a8f34e0e235ecb9f6d37b32be94bef90a505bd58
Yadvendra Naveen <yadvendranaveen@gmail.com>
2023-07-27 15:50:16 -0400

    Time: 54 ms (99.88%), Space: 17.6 MB (36.28%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hmap = {}
+        for i, num in enumerate(nums):
+            complement = target-num
+            if complement in hmap:
+                return [i, hmap[complement]]
+            hmap[num] = i
+        
