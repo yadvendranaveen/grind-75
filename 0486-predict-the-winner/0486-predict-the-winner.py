90750385fe3b9439e06c43f2a1a60ac3c03a4767
Yadvendra Naveen <yadvendranaveen@gmail.com>
2023-07-28 14:12:14 -0400

    Time: 34 ms (99.39%), Space: 16.3 MB (70.15%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,17 +1,11 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
         n = len(nums)
-        memo = defaultdict(int)
-
-        def maxDiff(left, right):
-            if (left, right) in memo:
-                return memo[(left, right)]
-            if left == right:
-                return nums[left]
-            score_by_left = nums[left] - maxDiff(left + 1, right)
-            score_by_right = nums[right] - maxDiff(left, right - 1)
-            memo[(left, right)] = max(score_by_left, score_by_right)
-
-            return memo[(left, right)]
+        dp = nums[:]
         
-        return maxDiff(0, n - 1) >= 0
+        for diff in range(1, n):
+            for left in range(n - diff):
+                right = left + diff
+                dp[left] = max(nums[left] - dp[left + 1], nums[right] - dp[left])
+        
+        return dp[0] >= 0
