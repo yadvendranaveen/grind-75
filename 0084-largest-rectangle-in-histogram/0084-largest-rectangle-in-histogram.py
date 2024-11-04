a8c69c3f8ce464df94591d3929d8461ab783d1d9
Yadvendra Naveen <yadvendranaveen@gmail.com>
2024-11-03 17:45:57 -0700

    Time: 278 ms (7.76%), Space: 34 MB (21.11%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -1,24 +1,23 @@
 class Solution:
-    def largestRectangleArea(self, heights):
-        stack = []
-        max_area = 0
-        heights.append(0)  # Add a sentinel value to flush out remaining bars in stack
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        n = len(heights)
+        left, right = [-1] * n, [n] * n
 
-        for i in range(len(heights)):
-            while stack and heights[i] < heights[stack[-1]]:
-                h = heights[stack.pop()]
-                w = i if not stack else i - stack[-1] - 1
-                print(i,heights[i])
-                if stack:
-                    print(stack[-1], i - stack[-1] - 1, i - stack[-1] )
-                print('-----')
-                max_area = max(max_area, h * w)
-            stack.append(i)
+        # Calculate left boundaries
+        for i in range(1, n):
+            prev = i - 1
+            while prev >= 0 and heights[prev] >= heights[i]:
+                prev = left[prev]
+            left[i] = prev
 
-        heights.pop() # Remove the sentinel value
-        return max_area
+        # Calculate right boundaries
+        for i in range(n - 2, -1, -1):
+            next = i + 1
+            while next < n and heights[next] >= heights[i]:
+                next = right[next]
+            right[i] = next
+        print(left, right, [heights[i] * (right[i] - left[i] - 1) for i in range(n)])
 
-
-        
-
-        
+        # Calculate the maximum area
+        return max(heights[i] * (right[i] - left[i] - 1) for i in range(n))
+            
