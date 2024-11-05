961b34d8ab5ea23b26c2e9f4e284c1f1932c1b33
Yadvendra Naveen <yadvendranaveen@gmail.com>
2024-11-05 12:31:30 -0700

    Time: 99 ms (56.89%), Space: 18.5 MB (6.47%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,10 +1,21 @@
-# Can be seen from examples A,A,A,B,B,B and A,A,A,B,B and A,A,A,B,B,B,C
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        counter_values = list(Counter(tasks).values())
-        max_count = max(counter_values)
-        max_freq_tasks = counter_values.count(max_count)
+        pq = list(map(lambda x: -x, Counter(tasks).values()))
+        heapify(pq)
 
-        slots = (max_count-1)*(n+1)+max_freq_tasks
-        return max(slots, len(tasks))
-        
+        time = 0
+        while pq:
+
+            cycle, task_count, stack = n+1, 0, []
+            while cycle>0 and pq:
+                freq = -heappop(pq)
+                if freq>1:
+                    stack.append( -(freq-1) )
+                cycle-=1
+                task_count+=1
+
+            pq = list(heapq.merge(pq, stack))
+            time += task_count if not pq else n+1 #add time taken for complete cycle if we expect tasks in next cycle, ie, pq not empty
+        return time
+            
+                
