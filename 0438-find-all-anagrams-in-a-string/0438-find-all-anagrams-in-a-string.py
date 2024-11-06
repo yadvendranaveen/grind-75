4a2b247b0be1c07eb31447676674f40230e5483b
Yadvendra Naveen <yadvendranaveen@gmail.com>
2024-11-05 23:34:00 -0700

    Time: 203 ms (33.68%), Space: 17.2 MB (83.5%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,22 +1,17 @@
 class Solution:
     def findAnagrams(self, s: str, p: str):
-        # Frequency map for characters in `p`
         p_count = Counter(p)
         s_count = Counter()
         
-        # Lengths of the strings
         len_s, len_p = len(s), len(p)
         result = []
         
         for i in range(len_s):
-            # Add current character to the window count
             s_count[s[i]] += 1
             
-            # Remove the leftmost character if the window size exceeds len_p
             if i >= len_p:
                 s_count[s[i - len_p]] -= 1
             
-            # Check if the current window matches `p`'s frequency map
             if s_count == p_count:
                 result.append(i - len_p + 1)
         
