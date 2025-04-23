2abea1c397c53af2f884eb3951d9919608dd0bcf
Yadvendra Naveen <yadvendranaveen@gmail.com>
2025-04-23 15:54:22 +0530

    Time: 3 ms (2.85%), Space: 17.8 MB (49%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,9 +1,11 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        @cache
+        memo = {}
         def backtrack(curr):
             if curr<0:  return 0
             if curr==0: return 1
-            return backtrack(curr-1)+backtrack(curr-2)
+            if curr in memo:    return memo[curr]
+            memo[curr] = backtrack(curr-1)+backtrack(curr-2)
+            return memo[curr]
 
         return backtrack(n)
