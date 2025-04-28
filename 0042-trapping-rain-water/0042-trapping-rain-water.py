0c48c0d4b1270ea93ebd5c98bc9bd9866cc4d68b
Yadvendra Naveen <yadvendranaveen@gmail.com>
2025-04-28 22:55:49 +0530

    Time: 31 ms (21.46%), Space: 19.5 MB (19.78%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,24 +1,14 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        n = len(height)
-        l2r, r2l = [0]*n, [0]*n
-        
-        u = height[0]
-        for i, h in enumerate(height):
-            if h > u:
-                u = h
-            else:
-                l2r[i] = u - h
-                
-        u = height[-1]
-        for i, h in reversed(list(enumerate(height))):
-            if h > u:
-                u = h
-            else:
-                r2l[i] = u - h
-        
-        return sum(map(min, zip(l2r, r2l)))
-        
-            
-                
-        
+        l2r = height[:]
+        for i in range(1, len(height)):
+            l2r[i] = max(l2r[i-1], l2r[i])
+        r2l = height[:]
+        for i in range(len(height)-2, -1, -1):
+            r2l[i] = max(r2l[i+1], r2l[i])
+
+        max_possible_water_heights = list(map(min, zip(l2r, r2l)))
+        ans = 0
+        for i in range(len(height)):
+            ans += max_possible_water_heights[i]-height[i]
+        return ans
