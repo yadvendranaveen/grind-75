735041579f16273e586061b26d163ba5d57a5657
Yadvendra Naveen <yadvendranaveen@gmail.com>
2025-05-06 11:16:11 +0530

    Time: 2088 ms (13.86%), Space: 18.1 MB (54.5%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -0,0 +1,9 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [1]*n
+        for i in range(n):
+            for j in range(i):
+                if nums[i]>nums[j]:
+                    dp[i] = max(dp[i], dp[j]+(nums[i]>nums[j]))
+        return max(dp)
