2e115139fa44e300cbb9047e9e7123a2afafe988
Yadvendra Naveen <yadvendranaveen@gmail.com>
2025-05-10 09:39:19 +0530

    Time: 134 ms (22.71%), Space: 26.8 MB (94.91%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,11 +1,18 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        l2r_min = list(accumulate(prices, min))
-        r2l_max = list(accumulate(reversed(prices), max))[::-1]
+        # l2r_min = list(accumulate(prices, min))
+        # r2l_max = list(accumulate(reversed(prices), max))[::-1]
+        # ans = 0
+        # for l,h in zip(l2r_min, r2l_max):
+        #     ans = max(ans, h-l)
+        # return ans
+        min_yet = prices[0]
         ans = 0
-        for l,h in zip(l2r_min, r2l_max):
-            ans = max(ans, h-l)
+        for price in prices:
+            min_yet = min(min_yet, price)
+            ans = max(ans, price-min_yet)
         return ans
+
             
             
 
