01df0ede124efa4710bd702ed950212e75654046
Yadvendra Naveen <yadvendranaveen@gmail.com>
2025-08-12 18:49:59 -0700

    Time: 127 ms (52.45%), Space: 75.7 MB (8.78%) - LeetHub

--- 1023-time-based-key-value-store/1023-time-based-key-value-store.py
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.py
@@ -1,10 +1,17 @@
+from dataclasses import dataclass
+
+@dataclass
+class ValueObj:
+    value:str
+    timestamp:int
+
 class TimeMap:
 
     def __init__(self):
         self.store = defaultdict(list)
         
     def set(self, key: str, value: str, timestamp: int) -> None:
-        self.store[key].append((timestamp, value))
+        self.store[key].append(ValueObj(value, timestamp))
         
 
     def get(self, key: str, timestamp: int) -> str:
@@ -13,14 +20,14 @@ class TimeMap:
             l,r = 0, len(arr)
             while l<r:
                 mid = (l+r)//2
-                if arr[mid][0]<=timestamp:
+                if arr[mid].timestamp<=timestamp:
                     l = mid+1
                 else:
                     r = mid
             return l
         idx = upper_bound() 
-        return arr[idx-1][1] if idx else ""
-        # idx = bisect.bisect(self.store[key], timestamp, key=lambda x:x[0])
+        return arr[idx-1].value if idx else ""
+        # idx = bisect_right(self.store[key], timestamp, key=lambda x:x[0])
         # return self.store[key][idx-1][1] if idx else ""
         
 # Your TimeMap object will be instantiated and called as such:
